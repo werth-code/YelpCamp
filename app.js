6593c00fea0ee60ba29b39643e36a9feeeb4c5d5
Matt Werth <matthewwerth@gmail.com>
2020-08-05 11:08:04 -0400

    Added Navbar & Minor Styling

--- app.js
+++ app.js
@@ -9,15 +9,33 @@ app.set("view engine", "ejs")
     let campgrounds = [
       {
         name: "Salmon Creek",
-        image:"https://inteng-storage.s3.amazonaws.com/img/iea/MRw4y5ABO1/sizes/camping-tech-trends_resize_md.jpg"
+        image:
+          "https://inteng-storage.s3.amazonaws.com/img/iea/MRw4y5ABO1/sizes/camping-tech-trends_resize_md.jpg"
       },
       {
         name: "Bear Creek",
-        image: "https://fpdcc.com/wp-content/uploads/2018/05/camping-feature.jpg"
+        image:
+          "https://fpdcc.com/wp-content/uploads/2018/05/camping-feature.jpg"
+      },
+      {
+        name: "Bear Creek",
+        image:
+          "https://fpdcc.com/wp-content/uploads/2018/05/camping-feature.jpg"
+      },
+      {
+        name: "Bear Creek",
+        image:
+          "https://fpdcc.com/wp-content/uploads/2018/05/camping-feature.jpg"
+      },
+      {
+        name: "Bear Creek",
+        image:
+          "https://fpdcc.com/wp-content/uploads/2018/05/camping-feature.jpg"
       },
       {
         name: "Puma Creek",
-        image: "https://www.clickondetroit.com/resizer/TKok3oQdJk7boUfr9XljSKMmz7c=/886x498/smart/filters:format(jpeg):strip_exif(true):strip_icc(true):no_upscale(true):quality(65)/cloudfront-us-east-1.images.arcpublishing.com/gmg/BQLUDDAS4BGJJJ3MEEA3MODYGU.jpg"
+        image:
+          "https://www.clickondetroit.com/resizer/TKok3oQdJk7boUfr9XljSKMmz7c=/886x498/smart/filters:format(jpeg):strip_exif(true):strip_icc(true):no_upscale(true):quality(65)/cloudfront-us-east-1.images.arcpublishing.com/gmg/BQLUDDAS4BGJJJ3MEEA3MODYGU.jpg"
       }
     ];
 
