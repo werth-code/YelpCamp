f6891f02da2eb3b770f612e8201e95d62d58661b
Matt Werth <matthewwerth@gmail.com>
2020-08-27 07:47:04 -0400

    Edit And Delete Now Only Available To Owner - Logged In

--- routes/comments.js
+++ routes/comments.js
@@ -34,7 +34,7 @@ router.post("/", isLoggedIn, (req, res) => {
 
 //Comments Edit Route
 
-router.get("/:comment_id/edit", ( req, res ) => {
+router.get("/:comment_id/edit", checkCommentOwnership, ( req, res ) => {
   Comment.findById(req.params.comment_id, (err, foundComment) => {
     if(err) res.redirect("back")
     else {
@@ -45,7 +45,7 @@ router.get("/:comment_id/edit", ( req, res ) => {
 
 //Comments Update Route
 
-router.put("/:comment_id", (req, res) => {
+router.put("/:comment_id", checkCommentOwnership, (req, res) => {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComments) => {
     if (err) res.redirect("back")
     else res.redirect("/campgrounds/" + req.params.id)
@@ -54,7 +54,7 @@ router.put("/:comment_id", (req, res) => {
 
 //Comments Destroy Route
 
-router.delete("/:comment_id", (req, res) => {
+router.delete("/:comment_id", checkCommentOwnership, (req, res) => {
   Comment.findByIdAndRemove(req.params.comment_id, (err) => {
     if(err) res.redirect("back")
     else res.redirect("/campgrounds/" + req.params.id)
@@ -62,6 +62,24 @@ router.delete("/:comment_id", (req, res) => {
 })
 
 //Middleware
+
+//Make Sure Authorized To Edit!
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+      Comment.findById(req.params.comment_id, (err, foundComment) => {
+        if (err) res.redirect("back");
+        else {
+          if (foundComment.author.id.equals(req.user._id)) { //Special mongoose method!
+            next();
+          } else {
+            res.redirect("back");
+          }
+        }
+      });
+    } else res.redirect("back");
+  }
+
+//Make Sure User Is Logged In!
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
